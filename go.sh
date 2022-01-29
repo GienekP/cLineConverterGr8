afff94eb7d095bdf5b8115e99a387bc19f04c927
GienekP <97046310+GienekP@users.noreply.github.com>
2022-01-29 16:58:13 +0100

    Add files via upload

---
+++ go.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+convert -depth 8 picture.png rgb:picture.rgb
+./rgb2atari picture.rgb ataripal.raw 20
+rm picture.rgb
+convert -colors 2 -monochrome -type bilevel mono.png gray:mono.raw
+./lineconvg8 ataripal.raw mono.raw picture.asm 2C
+rm ataripal.raw
+rm mono.raw
+./mads picture.asm -o:picture.xex
