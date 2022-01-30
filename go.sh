0a195aa8aee486047a2843091f50ef3a01665b63
GienekP <97046310+GienekP@users.noreply.github.com>
2022-01-30 10:41:51 +0100

    Add files via upload

--- go.sh
+++ go.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 convert -depth 8 picture.png rgb:picture.rgb
-rgb2atari picture.rgb ataripal.raw 20
+rgb2atari picture.rgb ataripal.raw 40
 rm picture.rgb
-convert -colors 2 -monochrome -type bilevel mono.png gray:mono.raw
+convert -colors 8 gray.png gray:mono.raw
 lineconvg8 ataripal.raw mono.raw picture.asm 2C
 rm ataripal.raw
 rm mono.raw
