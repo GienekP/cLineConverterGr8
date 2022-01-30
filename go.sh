316048d7eec5ec377fec85299fc636099a02e0c1
GienekP <97046310+GienekP@users.noreply.github.com>
2022-01-30 10:42:58 +0100

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,7 +4,7 @@ convert -depth 8 picture.png rgb:picture.rgb
 rgb2atari picture.rgb ataripal.raw 40
 rm picture.rgb
 convert -colors 8 gray.png gray:mono.raw
-lineconvg8 ataripal.raw mono.raw picture.asm 2C
+lineconvg8 ataripal.raw mono.raw picture.asm 2
 rm ataripal.raw
 rm mono.raw
 mads picture.asm -o:picture.xex
